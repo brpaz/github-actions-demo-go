7ec6f55d26db0844c008d65f10cba34c359c3f3f
Yunkon (Alvin) Kim <hermitkim1@users.noreply.github.com>
2020-09-01 17:50:25 +0900

    Update hello_test.go

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -5,6 +5,6 @@ import "testing"
 func TestGreetsGitHub(t *testing.T) {
 	result := Greet()
 	if result != "Hello GitHub Actions. Dev.to is awesome" {
-		t.Errorf("Greet() = %s; want Hello GitHub Actions. Dev.to is awesome", result)
+		t.Errorf("Greet() = %s; want Hello GitHub Actions. Dev.to is pretty awesome", result)
 	}
 }
