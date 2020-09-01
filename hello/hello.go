7ea60081e0bda2413a3513285f7e73f28b127f5d
Yunkon (Alvin) Kim <hermitkim1@users.noreply.github.com>
2020-09-01 17:51:11 +0900

    Update hello.go

--- hello/hello.go
+++ hello/hello.go
@@ -2,5 +2,5 @@ package hello
 
 // Greet Greets GitHub Actions
 func Greet() string {
-	return "Hello GitHub Actions. Dev.to is pretty awesome"
+	return "Hello GitHub Actions. Dev.to is preetty awesome"
 }
